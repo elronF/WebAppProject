4939b00d69a667551bbb488706e48096bbcb1071
vagrant <vagrant@vagrant.vm>
2018-10-18 03:19:49 +0000

    Connected main app to DB

--- acctTracker/app.py
+++ acctTracker/app.py
@@ -12,29 +12,12 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-account = {'id': '1', 'owner': 'Logan', 'accountType': 'RRSP', 'institution': 'RBC'}
-accounts = [{'id': '1', 'owner': 'Logan', 'accountType': 'RRSP', 'institution': 'RBC'}, 
-            {'id': '2', 'owner': 'Madeleine', 'accountType': 'RRSP', 'institution': 'RBC'}, 
-            {'id': '3', 'owner': 'Logan', 'accountType': 'TFSA', 'institution': 'RBC'}, 
-            {'id': '4', 'owner': 'Madeleine', 'accountType': 'TFSA', 'institution': 'RBC'}]
-
-stock = {'id': '1', 'ticker': 'CJ', 'exchange': 'TSX', 'companyName': 'Cardinal Energy', 'industry': 'Energy'}
-stocks = [{'id': '1', 'ticker': 'CJ', 'exchange': 'TSX', 'companyName': 'Cardinal Energy', 'industry': 'Energy'}, 
-          {'id': '2', 'ticker': 'ACB', 'exchange': 'TSX', 'companyName': 'Aurora Cannabis', 'industry': 'Cannabis'},
-          {'id': '3', 'ticker': 'GOOG', 'exchange': 'NASDAQ', 'companyName': 'Alphabet Inc', 'industry': 'Technology'}]
-
-
 # Show all accounts
 @app.route('/')
 @app.route('/accounts')
 def showAccounts():
+    accounts = session.query(Account).all()
     return render_template('accounts.html', accounts=accounts)
-
-# Show all accounts of one owner
-@app.route('/accounts/<string:owner>/')
-def showOwnerAccounts(owner):
-
-
 
 
 if __name__ == '__main__':
