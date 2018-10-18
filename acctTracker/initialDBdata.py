f8875637adcd15f44e4322b1e1ec4f8c559f31c7
vagrant <vagrant@vagrant.vm>
2018-10-18 03:21:58 +0000

    Final touchups for setting up DB

--- acctTracker/initialDBdata.py
+++ acctTracker/initialDBdata.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from db_setup import Owner, Account, Stock, Base
+
 engine = create_engine('sqlite:///tracker.db')
 Base.metadata.bind = engine
 
@@ -18,14 +20,18 @@ account1 = Account(accountType="RRSP", institution="RBC", owner_name=owner1.name
 account2 = Account(accountType="TFSA", institution="RBC", owner_name=owner1.name)
 account3 = Account(accountType="NonReg", institution="RBC", owner_name=owner1.name)
 
-session.add(account1, account2, account3)
+session.add(account1)
+session.add(account2)
+session.add(account3)
 session.commit()
 
 stock1 = Stock(ticker="CJ", exchange="TSX", companyName="Cardinal Energy", industry="Energy", description="Medium sized oil and gas company with assets in Canada", account=account1)
 stock2 = Stock(ticker="GOOG", exchange="NYSE", companyName="Alphabet Inc.", industry="Technology", description="An American multinational conglomerate headquartered in Mountain View, California.", account=account2)
 stock3 = Stock(ticker="ACB", exchange="TSX", companyName="Aurora Cannabis", industry="Cannabis", description="Major Canadian cannabis producer with operations focused in Alberta", account=account3)
 
-session.add(stock1, stock2, stock3)
+session.add(stock1)
+session.add(stock2)
+session.add(stock3)
 session.commit()
 
 # Data second owner
@@ -38,13 +44,18 @@ account1 = Account(accountType="RRSP", institution="TD", owner_name=owner2.name)
 account2 = Account(accountType="TFSA", institution="BMO", owner_name=owner2.name)
 account3 = Account(accountType="NonReg", institution="Questrade", owner_name=owner2.name)
 
-session.add(account1, account2, account3)
+session.add(account1)
+session.add(account2)
+session.add(account3)
+session.commit()
 
 stock1 = Stock(ticker="TOU", exchange="TSX", companyName="Tourmaline Oil and Gas", industry="Energy", description="Large cap natural gas producer with assets in Alberta and BC", account=account1)
 stock2 = Stock(ticker="WEED", exchange="TSX", companyName="Canopy Growth.", industry="Cannabis", description="Major Canadian cannabis producer with operations focused in Ontario", account=account2)
 stock3 = Stock(ticker="FAKE", exchange="TSX", companyName="Fake Corp.", industry="Stuff", description="A real company doing real things", account=account3)
 
-session.add(stock1, stock2, stock3)
+session.add(stock1)
+session.add(stock2)
+session.add(stock3)
 session.commit()
 
 # Data third owner
@@ -56,14 +67,17 @@ session.commit()
 account1 = Account(accountType="RESP", institution="CIBC", owner_name=owner3.name)
 account2 = Account(accountType="TFSA", institution="Questrade", owner_name=owner3.name)
 
-session.add(account1, account2)
+session.add(account1)
+session.add(account2)
 session.commit()
 
 stock1 = Stock(ticker="ATVI", exchange="NYSE", companyName="Activision Blizzard Inc.", industry="Entertainment", description="An American video game holding company based in Santa Monica, California", account=account1)
 stock2 = Stock(ticker="FB", exchange="NYSE", companyName="Facebook Inc.", industry="Technology", description="An American online social media and social networking service company based in Menlo Park, California", account=account2)
 stock3 = Stock(ticker="AAPL", exchange="NYSE", companyName="Apple Inc.", industry="Technology", description="An American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services.", account=account2)
 
-session.add(stock1, stock2, stock3)
+session.add(stock1)
+session.add(stock2)
+session.add(stock3)
 session.commit()
 
 print("Added owners, accounts and stocks!")
