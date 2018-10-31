6e54e28a8f308ce7588f14bbb3913161d1f0a14f
vagrant <vagrant@vagrant.vm>
2018-10-31 03:38:14 +0000

    Fixed google connect issues

--- acctTracker/app.py
+++ acctTracker/app.py
@@ -9,8 +9,13 @@ import random, string # allows us to create pseudo-random string to identify a s
 
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
+import httplib2
+import socks
 import json
+from flask import make_response
+import requests
 
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
 
 app = Flask(__name__)
 
@@ -22,12 +27,141 @@ session = DBSession()
 
 
 # OAUTH LOGIC
-# Create state token. Store in session.
+'''Create state token. Store in session.'''
 @app.route('/login/')
+#@app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
     login_session['state'] = state
     return render_template('login.html', STATE=state)
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    '''Validate state token'''
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    '''Get auth code'''
+    code = request.data
+    try:
+        '''upgrade auth code into credentials object'''
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    ''' Check validity of access token'''
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    #url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}').format(access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url,'GET')[1].decode('utf-8'))
+    print(result)
+    
+    '''Abort if access token is invalid'''
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    '''Verify access token is used for intended user'''
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps("Token's user ID doesn't match given user ID"), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    '''Verify that access token is valid for app'''
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(json.dumps("Token's client ID doesn't match app's client ID"), 401)
+        print("Token's client ID doesn't match app's")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    '''Check to see if user already logged in'''
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user already logged-in.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    '''Store access token for later use'''
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    '''Get user info'''
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+
+    login_session['username'] = data["name"]
+    #login_session['picture'] = data["picture"]
+    login_session['email'] = data["email"]
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    flash("you are now logged in as {}".format(login_session['username']))
+    print("done!")
+    return output
+
+# DISCONNECT - Revoke a current user's token and reset their login_session
+# To disconnect from google login
+@app.route('/gdisconnect')
+def gdisconnect():
+    # Only disconnect a connected user.
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    if result['status'] == '200':
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for \
+        given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+# USER FUNCTIONS
+# Returns a user ID if there's a match based on the email passed into it.
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+       return None
+
+
+# If userid passed in, returns user object associated with user number
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+# creates a new user in the DB based on name and email, returns an ID.
+def createUser(login_session):
+    newUser = User(name = login_session['username'], email = login_session['email'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
 
 
 # ROUTES-NO LOGIN
@@ -126,31 +260,6 @@ def accountsJSON():
 def stockJSON(account_id, stock_ticker):
     stock = session.query(Stock).filter_by(ticker=stock_ticker).one()
     return jsonify(stock=stock.serialize)
-
-
-# USER FUNCTIONS
-# Returns a user ID if there's a match based on the email passed into it.
-def getUserID(email):
-    try:
-        user = session.query(User).filter_by(email = email).one()
-        return user.id
-    except:
-       return None
-
-
-# If userid passed in, returns user object associated with user number
-def getUserInfo(user_id):
-    user = session.query(User).filter_by(id = user_id).one()
-    return user
-
-
-# creates a new user in the DB based on name and email, returns an ID.
-def createUser(login_session):
-    newUser = User(name = login_session['username'], email = login_session['email'])
-    session.add(newUser)
-    session.commit()
-    user = session.query(User).filter_by(email = login_session['email']).one()
-    return user.id
 
 
 if __name__ == '__main__':
